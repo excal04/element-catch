e1736fc9b4c1be97d31d1cff18c6fde3ca886157
excal04 <excal04@users.noreply.github.com>
2015-07-20 17:34:48 +0800

    catcher working, needs improvement

--- elem-catch.js
+++ elem-catch.js
@@ -1,11 +1,28 @@
 
 // named elementCatch to stay consistent with the original
-HTMLElement.prototype.elementCatch = function() {
-    console.log("hi");
-}
+HTMLElement.prototype.elementCatch = elementCatch;
+
+function elementCatch(scrolledClass) {
+    scrolledClass = scrolledClass || "scrolled";
+
+    var $this = this;
+
+    var distFromTop = $this.offsetTop;
+
+    var scrollPosition = document.body.scrollTop;
+
+    if (scrollPosition > distFromTop) {
+        $this.classList.add(scrolledClass);
+    } else {    // element is within page
+        $this.classList.remove(scrolledClass);
+    }
 
 
-window.onscroll = function(ev) {
-    var elem = document.querySelector(".menu");
-    elem.elementCatch();
+
+    return this;
 }
+//
+// window.onscroll = function(ev) {
+//     var elem = document.querySelector(".menu");
+//     elem.elementCatch();
+// }
